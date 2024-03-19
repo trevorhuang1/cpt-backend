d69fab851908f59e110d0abb69f277bf12512e2d
trevorhuang1 <trevorhuangsd@gmail.com>
2024-03-19 09:16:01 -0700

    there is one error left

--- api/titanic.py
+++ api/titanic.py
@@ -1,32 +1,23 @@
-import json, jwt
 from flask import Blueprint, request, jsonify, current_app, Response
-from flask_restful import Api, Resource
-from datetime import datetime
-from model.titanics import TitanicRegression, predictSurvival
-import pandas as pd
-import seaborn as sns
-import numpy as np
+from flask_restful import Api, Resource # used for REST API building
 
-# Define the Titanic API blueprint
-titanic_api = Blueprint("titanic_api", __name__, url_prefix="/api/titanic")
+from model.titanics import TitanicRegression
+
+titanic_api = Blueprint('titanic_api', __name__,
+                   url_prefix='/api/titanic')
+
+# API docs https://flask-restful.readthedocs.io/en/latest/api.html
 api = Api(titanic_api)
 
-class TitanicAPI:
-    class Passenger(Resource):
-        def get(self):
-            passenger = pd.DataFrame({
-                'name': ['John Mortensen'],
-                'pclass': [2],  # 2nd class picked as it was median, bargains are my preference, but I don't want to have poor accomodations
-                'sex': ['male'],
-                'age': [64],
-                'sibsp': [1],  # I usually travel with my wife
-                'parch': [1],  # currently I have 1 child at home
-                'fare': [16.00],  # median fare picked assuming it is 2nd class
-                'embarked': ['S'],  # majority of passengers embarked in Southampton
-                'alone': [False]  # travelling with family (spouse and child)
-            })
+# Initialize the model
+titanic_model = TitanicRegression()
 
-            response = predictSurvival(self=self, passenger=passenger)  # Assuming predictSurvival is defined elsewhere
-            return response
+class TitanicApi:        
+    class _CRUD(Resource): 
+        def post(self):
+            data = request.get_json()
+            # print(data)
+            alive_prob = titanic_model.predictSurvival(data)
+            return jsonify({'survival_probability': alive_prob})
 
-    api.add_resource(Passenger, "/")  # Register Passenger resource
+    api.add_resource(_CRUD, '/')
--- model/titanics.py
+++ model/titanics.py
@@ -20,6 +20,8 @@ class TitanicRegression:
         self.y_train = None
         self.y_test = None
         self.encoder = None
+        self.initTitanic()  # Call initTitanic in the constructor
+
 
     def initTitanic(self):
         titanic_data = sns.load_dataset('titanic')
@@ -41,6 +43,7 @@ class TitanicRegression:
         # clean data
 
 
+
     def runDecisionTree(self):
         X = self.td.drop('survived', axis=1) # all except 'survived'
         y = self.td['survived'] # only 'survived'
@@ -52,35 +55,57 @@ class TitanicRegression:
 
         # more code here
 
-    def runLogisticRegression(self):
+    def runLogisticRegression(self, X, y):
         # more code here
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.3, random_state=42)
         self.logreg = LogisticRegression()
         self.logreg.fit(self.X_train, self.y_train)
+        
+    def predictSurvival(self, data):
+        passenger = data.get("passenger")
+        new_passenger = {
+            'name': passenger.get('name'),
+            'pclass': passenger.get('pclass'),
+            'sex': passenger.get('sex'),
+            'age': passenger.get('age'),
+            'sibsp': passenger.get('sibsp'),
+            'parch': passenger.get('parch'),
+            'fare': passenger.get('fare'),
+            'embarked': passenger.get('embarked'),
+            'alone': passenger.get('alone')
+        }
+
+        # Preprocess the new passenger data
+        new_passenger['sex'] = 1 if new_passenger['sex'] == 'male' else 0
+        new_passenger['alone'] = 1 if new_passenger['alone'] else 0
+
+        # Encode 'embarked' variable
+        embarked_value = new_passenger['embarked']
+        embarked_encoded = self.enc.transform(np.array([embarked_value]).reshape(1, -1))
+        print(new_passenger)
+
+        cols = ['embarked_' + val for val in self.enc.categories_[0]]
+        print(new_passenger)
+        new_passenger.update({col: value for col, value in zip(cols, embarked_encoded.toarray()[0])})
+
+        # Drop unnecessary columns
+        new_passenger_df = pd.DataFrame(new_passenger)
+        new_passenger_df.drop(['name', 'embarked'], axis=1, inplace=True)
+
+        aliveProb = self.logreg.predict_proba(new_passenger_df)[:, 1]
+
+        return aliveProb
+
 
 def initTitanic():
     global titanic_regression
     titanic_regression = TitanicRegression()
     titanic_regression.initTitanic()
+    X = titanic_regression.td.drop('survived', axis=1)
+    y = titanic_regression.td['survived']
+    titanic_regression.runLogisticRegression(X, y)  #s  # Pass X and y to runLogisticRegression
+
 
 # From API
-def predictSurvival(self,passenger):
-    new_passenger = passenger.copy()
-    # Preprocess the new passenger data
-    new_passenger['sex'] = new_passenger['sex'].apply(lambda x: 1 if x == 'male' else 0)
-    new_passenger['alone'] = new_passenger['alone'].apply(lambda x: 1 if x == True else 0)
 
-    # Encode 'embarked' variable
-    self.onehot = self.enc.transform(new_passenger[['embarked']]).toarray()
-    cols = ['embarked_' + val for val in self.enc.categories_[0]]
-    new_passenger[cols] = pd.DataFrame(self.onehot, index=new_passenger.index)
-    new_passenger.drop(['name'], axis=1, inplace=True)
-    new_passenger.drop(['embarked'], axis=1, inplace=True)
-    aliveProb = np.squeeze(self.logreg.predict_proba(new_passenger))
-
-    # more code here
-    # interact with 
-    return aliveProb
 # Sample usage without API
-if __name__ == "__main__":
-    # Initialize the Titanic model
-    initTitanic()
