ed0740dba03d6db2f8354a063b2e923a13b172db
trevorhuang1 <trevorhuangsd@gmail.com>
2024-03-28 12:14:18 -0700

    fixed inflation model (forgot to commit)

--- model/inflation.py
+++ model/inflation.py
@@ -1,56 +1,71 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, export_text
 from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import accuracy_score
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+import pandas as pd
+import numpy as np
 import random
 
-class InflationModel:
+inflation_regression = None
 
-    _instance = None
-    
+class InflationModel:
     def __init__(self):
-        self.model = None
         self.dt = None
-        self.inflation_data = pd.read_csv('datasets/US_CPI.csv', header=None)
-        self.features = ['Date']
-        self.target = ['CPI']
-        self.encoder = OneHotEncoder(handle_unknown='ignore')
-    def cleanInflation(self):
-        self.inflation_data['CPI'] = pd.to_numeric(self.inflation_data['CPI'], errors='coerce')
-        self.inflation_data = self.inflation_data.dropna(subset=['CPI'])
-        self.inflation_data['Date'] = pd.to_datetime(self.inflation_data['Date'], formate='%d-%m-%Y', errors='coerce')
-        self.inflation_data['Date'] = self.inflation_data['Date'] + pd.offsets.MonthEnd(0)
-        self.inflation_data['Timestamp'] = (self.inflation_data['Date']-pd.Timestamp('1970-01-01'))
-        self.inflation_data.dropna(inplace=True)
-        self.inflation_data['Inflation Rate'] = self.inflation_data['CPI'].pct_change() * 100
+        self.model = None
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
+        self.encoder = None
+        self.initInflation()
     
-    def trainInflation(self):
-        X = self.inflation_data[self.features]
-        y = self.inflation_data[self.target]
+    def initInflation(self):
+        inflation_data = pd.read_csv('datasets/us_cpi.csv', header=None)
+        self.td = inflation_data
+        self.td.columns = ['Date', 'CPI']
         
-        self.model = LogisticRegression(max_iter=1000)
+        self.td['CPI'] = pd.to_numeric(self.td['CPI'], errors='coerce')
+        self.td = self.td.dropna(subset=['CPI'])
         
-        self.model.fit(X,y)
-        self.dt.fit(X, y)
-    
-    @classmethod
-    def get_instance(cls):
-        if cls._instance is None:
-            cls._instance = cls()
-            cls._instance.cleanInflation()
-            cls._instance.trainInflation()
-        return cls._instance
+        #convert yearmon to datetime objects
+        self.td['Date'] = pd.to_datetime(self.td['Date'], format='%d-%m-%Y', errors='coerce')
+        self.td['Date'] = self.td['Date'] + pd.offsets.MonthEnd(0)
+        self.td['Timestamp'] = (self.td['Date'] - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
+
+        #Inflation rate calculation
+        self.td['Inflation Rate'] = self.td['CPI'].pct_change() * 100
+        
+        self.td = self.td.dropna(subset=['Inflation Rate'])
+
+    def runLinearRegression(self, X, y):
+        X = self.td['Timestamp'].values.reshape(-1,1)
+        y = self.td['Inflation Rate'].values
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.model = LinearRegression()
+        self.model.fit(self.X_train, self.y_train)
+        
+        self.y_pred = self.model.predict(self.X_test)
+        
     def predictInflation(self, date):
-        future_date = pd.to_datetime(date, format='%Y-%m-%d')
-        future_timestamp = (future_date - pd.Timestamp("1970-01-91")) // pd.Timedelta('1s')
-        future_inflation_rate = self.model.predict([[future_timestamp]])
-        randomInt = random.randint(1,40)
+        X = self.td['Timestamp'].values.reshape(-1,1)
+        y = self.td['Inflation Rate'].values
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.model = LinearRegression()
+        self.model.fit(self.X_train, self.y_train)
+        future_date = pd.to_datetime(date["date"], format='%Y-%m-%d')
+        future_timestamp = (future_date - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
+        future_inflation_rate = self.model.predict(np.array([future_timestamp]).reshape(1,-1))
+        
+        randomInt = random.randint(1,50)
+        
         if randomInt == 1:
             future_inflation_rate[0]*=2
         return future_inflation_rate[0]*10
 def initInflation():
-    InflationModel.get_instance()
+    global inflation_regression
+    inflation_regression = InflationModel()
+    inflation_regression.initInflation()
+    X = inflation_regression.td['Timestamp']
+    y = inflation_regression.td['Inflation Rate']
+    inflation_regression.runLinearRegression(X, y)
